8029ead40f1297de5af9400a7c6b221a43b9d521
Prathap <itstillon@gmail.com>
2022-08-11 14:47:18 +0530

    Added level change event handler

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,17 @@ import pino from "pino";
 const logger = pino({
     base: { author: "itstillon" },
     formatters:{
-        level: label => ({ label })
+        level: (label: string) => ({ label })
     }
+});
+
+logger.on('level-change', (lvl: string, val: number, prevLvl: string, prevVal: number) => {
+    if (logger !== this) {
+        console.log("Child logger");
+        return;
+    }
+
+    console.log('%s (%d) was changed to %s (%d)', prevLvl, prevVal, lvl, val)
 });
 
 export default logger;
